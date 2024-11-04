657b78621df1006d2d5aad8a58117f850a2dcf6a
374688995 <374688995@qq.com>
2024-11-04 22:56:56 +0800

    个人电脑

--- src/posts/posts.entity.ts
+++ src/posts/posts.entity.ts
@@ -1,15 +1,28 @@
-import { Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 /*
  * @Description:TypeORM是通过实体映射到数据表，所以要建立一个文章实体posts.entity.ts
  * @Author: yangsen
  * @Date: 2024-11-03 22:27:54
  * @LastEditors: yangsen
- * @LastEditTime: 2024-11-03 22:38:05
+ * @LastEditTime: 2024-11-04 21:46:22
  */
 @Entity()
 export class PostsEntity {
   @PrimaryGeneratedColumn()
   id: number; // 标记为主键，值自动生成
-  
+  @Column({ length: 50 })
+  title: string;
+  @Column({ length: 20 })
+  author: string;
+  @Column('text')
+  content: string;
+  @Column({ default: '' })
+  thumb_url: string;
+  @Column('tinyint')
+  type: number;
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  create_date: Date;
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  update_date: Date;
 }
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,75 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { PostsEntity } from './posts.entity';
+import { Repository } from 'typeorm';
+import { error } from 'console';
 
+interface PostRo {
+  list: PostsEntity[];
+  count: number;
+}
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  constructor(
+    @InjectRepository(PostsEntity)
+    private readonly postsRepository: Repository<PostsEntity>,
+  ) {}
+
+  /* 创建文章 */
+  async create(post: Partial<PostsEntity>): Promise<PostsEntity> {
+    const { title } = post;
+    if (!title) {
+      throw new HttpException('文章标题不能为空', 401);
+    }
+    const doc = await this.postsRepository.findOne({
+      where: { title },
+    });
+    if (doc) {
+      throw new HttpException('文章标题已存在', 401);
+    }
+
+    return await this.postsRepository.save(post);
+  }
+
+  /**  获取文章列表 */
+  async findAll(query): Promise<PostRo> {
+    const qb = this.postsRepository.createQueryBuilder('post');
+    qb.where('1=1');
+    qb.orderBy('post.create_time', 'DESC'); // DESC是降序的意思，升序：ASC
+
+    const count = await qb.getCount();
+    const { pageNum = 1, pageSize = 10, ...params } = query;
+    qb.limit(pageSize); // 可以选择的最大行数
+    qb.offset((pageNum - 1) * pageSize);
+
+    const posts = await qb.getMany();
+    return {
+      list: posts,
+      count,
+    };
+  }
+
+  /** 获取指定文章 */
+  async findById(id): Promise<PostsEntity> {
+    return await this.postsRepository.findOne(id);
+  }
+
+  /** 更新文章 */
+  async updateById(id, post): Promise<PostsEntity> {
+    const existPost = await this.postsRepository.findOne(id);
+    if (!existPost) {
+      throw new HttpException(`id为${id}的文章不存在`, 401);
+    }
+    const updatePost = this.postsRepository.merge(existPost, post);
+    return this.postsRepository.save(updatePost);
+  }
+
+  /** 删除文章 */
+  async removeById(id): Promise<any> {
+    const existPost = await this.postsRepository.findOne(id);
+    if (!existPost) {
+      throw new HttpException(`id为${id}的文章不存在`, 401);
+    }
+    return await this.postsRepository.delete(id);
+  }
+}
