628e60b3f017b42410c9ceeb975c3b12f96e2676
374688995@qq.com <374688995@qq.com>
2024-11-07 18:36:32 +0800

    司空电脑

---
+++ src/core/filter/http-exception/http-exception.filter.ts
@@ -0,0 +1,33 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+} from '@nestjs/common';
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+  /* exception: 捕获到的 HTTP 异常对象。
+  host: 当前请求的上下文对象，包含请求和响应的信息。 */
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp(); // 获取请求上下文
+    const response = ctx.getResponse(); // 获取请求上下文中的 response对象
+    const status = exception.getStatus(); // 获取异常状态码
+
+    // 设置错误信息
+    const message = exception.message
+      ? exception.message
+      : `${status >= 500 ? 'Server Error' : 'Client Error'}`;
+    const errorResponse = {
+      data: {},
+      message,
+      code: -1,
+      request: `${ctx.getRequest().method} ${ctx.getRequest().url}`,
+    };
+
+    // 设置返回的状态码， 请求头，发送错误信息
+    response.status(status);
+    response.header('Content-Type', 'application/json; charset=utf-8');
+    response.send(errorResponse);
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './core/filter/http-exception/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // 注册全局错误的过滤器
+  app.useGlobalFilters(new HttpExceptionFilter());
   // process是nodejs的全局变量
   await app.listen(process.env.PORT ?? 3001);
 }
